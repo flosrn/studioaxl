f141b2b04460d680edc7f7538ce841e437ade461
Flosrn <flosrn.dev@gmail.com>
2021-01-19 21:18:34 +0100

    cursor

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { GetStaticProps } from "next";
 import { useI18n, I18nProps } from "next-rosetta";
+import { motion, useMotionValue } from "framer-motion";
 
 import Header from "components/ui/header";
 import Arrow from "components/ui/animations/arrow";
-import { MyLocale } from "i18n";
 import Meaning from "components/ui/animations/meaning";
 import Empathy from "components/ui/animations/empathy";
 
+import { MyLocale } from "i18n";
+
 const HomePage: React.FC = () => {
   const { t } = useI18n<MyLocale>();
+  const [cursorXY, setCursorXY] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const animation = (e) => {
+      const x = e.clientX - 50;
+      const y = e.clientY - 50;
+      setCursorXY({ x, y });
+    };
+    // const setFromEvent = (e) => setPosition({ x: e.clientX, y: e.clientY });
+    window.addEventListener("mousemove", animation);
+
+    // the function returned here will remove, or "clean up", the event listener
+    return () => {
+      window.removeEventListener("mousemove", animation);
+    };
+  }, []);
+
   return (
     <div className="bg-white">
+      <div
+        className="cursor"
+        style={{
+          transform: `translate3d(${cursorXY.x}px, ${cursorXY.y}px, 0)`,
+        }}
+      />
       <Header />
       <section className="h-screen flex flex-col justify-center items-center">
         <div className="flex justify-center items-end">
